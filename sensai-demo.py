3e874cc4fd1e6ec27d0ca83dadfbcfc58a204cd4
Rayan Nadeem <rayan.nadeem@outlook.com>
2023-07-06 15:30:28 +0100

    fixed demo

--- sensai-demo.py
+++ sensai-demo.py
@@ -7,8 +7,15 @@ app = Flask(__name__)
 def homepage():
     if request.method == 'POST':
         text = request.form.get("text")
-        evalution = sensai.sentiment_analysis(text)
-        label = "Polarity: " + evalution[0]["label"]
-        return render_template('demo.html', label=label)
+        evaluation = sensai.sentiment_analysis(text)
+        label = "Polarity: " + evaluation[0]["label"]
+        if evaluation[0]["label"] == "POSITIVE":
+            percent_positive="Positive: " + str(int(evaluation[0]["score"] * 100))
+            percent_negative="Negative: " + str(int(100 - (evaluation[0]["score" * 100])))
+        else:
+            percent_negative="Negative: " + str(int(evaluation[0]["score"] * 100))
+            percent_positive="Positive: " + str(int(100 - (evaluation[0]["score" * 100])))
+        
+        return render_template('demo.html', label=label, percent_positive=percent_positive, percent_negative=percent_negative)
     label = ""
     return render_template('demo.html')
