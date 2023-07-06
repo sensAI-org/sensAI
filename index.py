046f754002c1f63d2289999d6b73727965ac5d84
Rayan Nadeem <rayan.nadeem@outlook.com>
2023-07-06 17:44:10 +0100

    added waitress

--- index.py
+++ index.py
@@ -1,21 +1,24 @@
 import sensai
 from flask import Flask, render_template, request
+from waitress import serve
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     if request.method == 'POST':
         text = request.form.get("text")
         evaluation = sensai.sentiment_analysis(text)
-        label = "Polarity: " + evaluation[0]["label"]
         if evaluation[0]["label"] == "POSITIVE":
-            percent_positive="Positive: " + str(int(evaluation[0]["score"] * 100))
-            percent_negative="Negative: " + str(int(100 - (evaluation[0]["score" * 100])))
+            percent_positive="Positive: " + str(int(evaluation[0]["score"] * 100)) + "%"
+            percent_negative="Negative: " + str(int(100 - (evaluation[0]["score"] * 100))) + "%"
         else:
-            percent_negative="Negative: " + str(int(evaluation[0]["score"] * 100))
-            percent_positive="Positive: " + str(int(100 - (evaluation[0]["score" * 100])))
+            percent_negative="Negative: " + str(int(evaluation[0]["score"] * 100)) + "%"
+            percent_positive="Positive: " + str(int(100 - (int(evaluation[0]["score"] *100)))) + "%"
         
-        return render_template('demo.html', label=label, percent_positive=percent_positive, percent_negative=percent_negative)
-    label = ""
+        return render_template('demo.html', percent_positive=percent_positive, percent_negative=percent_negative, text=request.form.get("text"))
     return render_template('demo.html')
+
+serve(app, host="0.0.0.0", port=8080)
+print("this is runnign")
